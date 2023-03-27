eb6fc65a9862a68dd5d9339fadc76f9af3c1df58
Uriel <urieazul907@gmail.com>
2023-03-27 02:43:49 -0600

    subiendo proyecto

---
+++ Conversor-de-Moneda/src/Main.java
@@ -0,0 +1,16 @@
+/*CREADOR: URIEL CRUZ VALDES
+
+        **REDES SOCIALES**
+        HitGub: Urielczv
+        twitter: @Uriel_cruzv
+        Discord: Uriel交 #6555
+        Correo: urieazul907@gmail.com*/
+public class Main {
+        public static void main(String[] args) {
+                //se crea una variable de tipo "funciones" para poder usar los metodos de esa clase
+                Funciones conversion = new Funciones();
+
+                //se manda a llamar al metodo "Menu_Principal" para iniciar el programa
+                conversion.Menu_Principal();
+        }
+}
